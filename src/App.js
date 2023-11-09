125d8c7ebd926c453b27f9d12ac9a71525f3f9c4
Ayham alia <ayhamalia1234@gmail.com>
2023-11-09 18:29:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
+import Login from './Component/Login/Login'
+import Dashboard from './Component/Dashboard/Dashboard'
+import ReviewHoliday from './Review_Request_Holiday/ReviewHoliday'
+import CreateAccount from './Component/Create_Employee_Account/CreateAccount'
+import QR from './Component/QR_Code/QR'
+import ActiveUsers from './Component/Active_Users/ActiveUsers'
+import UpdateUsers from './Component/Update_Users_Data/UpdateUsers'
+import NotFound from './Component/NotFound/NotFound'
+import Loader from './Component/Loader/Loader';
+import { jwtDecode} from 'jwt-decode';
+import ProtectedRoutes from './Component/ProtectedRoutes/ProtectedRoutes'
+import Edit_IPAddress from './Component/Edit_IPAddress/Edit_IPAddress'
+import Archive from './Component/Archive/Archive'
+import axios from 'axios'
 
-function App() {
+
+export default function App() {
+  let [companyData,setCompanyData]=useState(null);
+  let token=localStorage.getItem('token');
+  let Navigate=useNavigate();
+  let [employees,setEmployees]=useState([]);
+  let [loading,setLoading]=useState(true);
+  let [TotalPages, setTotalPages] = useState();
+  function getCompanyData(){
+    let decoded=jwtDecode(localStorage.getItem("token"));
+     setCompanyData(decoded);
+  }
+  //Pagination
+  const handlePageChange = (data) => {
+    setEmployees([]);
+    getEmployees(data.selected + 1);
+ };
+  //get all Employee
+  const getEmployees=async(curr)=>{
+    let {data}=await axios.get("https://staff-scanner.vercel.app/company/getEmployees",{headers:{"Authorization":`Bearer ${token}`},params: {
+      page: curr,
+      perPage:7,
+    }})
+    setEmployees(data.employees);
+    setTotalPages(data.totalPages);
+    setLoading(false);
+  }
+  // to Logout
+  function logout(){
+    localStorage.removeItem("token");
+    localStorage.setItem("isLogged", false);
+    setCompanyData(null);
+    Navigate("/login");
+  }
+  useEffect(()=>{
+    getEmployees();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<>
+{loading ?(<Loader/>):(
+<Routes>
+  <Route element={<ProtectedRoutes/>}>
+  <Route path='Dashboard' element={<Dashboard logout={logout} employees={employees} handlePageChange={handlePageChange} TotalPages={TotalPages}/>}></Route>
+  <Route path='ReviewHoliday' element={<ReviewHoliday/>}></Route>
+  <Route path='EditIPAddress' element={<Edit_IPAddress/>}></Route>
+  <Route path='CreateAccount' element={<CreateAccount/>}></Route>
+  <Route path='/Archive' element={<Archive/>}></Route>
+  <Route path='QRcode' element={<QR/>}></Route>
+  <Route path='ActiveUsers' element={<ActiveUsers/>}></Route>
+  <Route path='UpdateUsers/:id' element={<UpdateUsers />}></Route>
+  </Route>
+ <Route path='/' element={<Login getCompanyData={getCompanyData}/> } ></Route>
+ <Route path='*' element={<NotFound/>}></Route>
+</Routes>)}
+</>
+  )
 }
-
-export default App;
